1e264a7df3c9db621b33d5ed46e4babd49130572
Jeff Hieu Nguyen <jeffhieun@users.noreply.github.com>
2026-06-22 11:09:29 +0700

    create the full implement for reservation payment system: done

--- backend/BcryptCheck.java
+++
@@ -1,7 +0,0 @@
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-public class BcryptCheck {
-  public static void main(String[] args) {
-    BCryptPasswordEncoder e = new BCryptPasswordEncoder(10);
-    System.out.println(e.matches("password123","$2a$10$dXJ3SUicVNrDY2Z8VHMh.eC2xVRMLNWFvD0jGDbNMqfHVNmXy1h0a"));
-  }
-}
